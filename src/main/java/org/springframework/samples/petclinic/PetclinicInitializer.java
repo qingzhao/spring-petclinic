ce6951c22e397413de73621a3aa53f4d72f6ab4d
qingzhao <qingzhao198@126.com>
2018-09-29 15:31:55 +0800

    Update PetclinicInitializer.java
    ffff

--- src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicInitializer.java
@@ -69,7 +69,7 @@ public class PetclinicInitializer extends AbstractDispatcherServletInitializer {
 
     @Override
     protected Filter[] getServletFilters() {
-        // Used to provide the ability to enter Chinese characters inside the Owner Form
+        // Used to provide the ability to enter Chinese characters inside the Owner Form  ddd
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter("UTF-8", true);
         return new Filter[]{characterEncodingFilter};
     }
